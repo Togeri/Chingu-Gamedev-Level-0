2401249e8e8e707b424dea84da13f45ea9acdeaa
Klingefjord <oliver.klingefjord@hyperisland.se>
2017-04-11 20:13:37 +0200

    Tilemap added instead of fills

--- Bin/map.js
+++ Bin/map.js
@@ -1,3 +1,26 @@
+var tileSet = new Image();
+tileSet.src = "Assets/img/Tileset.png";
+
+////////// Begin create map when image is loaded. /////////
+// TODO: Better way to preload image?
+
+tileSet.onload = function() {
+
+// store tile data in 2 arrays. If you look at tileset, tileX and tileY would correspond to the coordinates of said tile.
+// TODO: Maybe do this with 2d array instead? Not sure how though
+
+var tileX = [],
+    tileY = [];
+
+for (var i = 0; i < tileSet.width/16; i++) {
+  tileX[i] = i * 16;
+}
+
+for (var i = 0; i < tileSet.height/16; i++) {
+  tileY[i] = i * 16;
+}
+
+
 // tile size
 var tileSize = canvas.height / 16; // Game map is 16 tiles high
 
@@ -6,50 +29,60 @@ var tileSize = canvas.height / 16; // Game map is 16 tiles high
 // screen is 16 x 20 tiles (640 x 800px of 40px each)
 
 var map = [
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,2,2,2,2,3,2,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,2,2,2,2,3,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,3,2,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,5,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,4,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
-// Loop that creates rectangles from map data
+// Loop that fills in tiles from map data
 
 for (var y = 0; y < map.length; y++) {
   for (var x = 0; x < map[y].length; x++) {
     switch (map[y][x]) {
-      case 0:
-        drawTile(x, y, "lightblue"); // sky
-        break;
       case 1:
-        drawTile(x, y, "green"); // grass
+        drawTile(x, y, tileX[0], tileY[0]); // ground
         break;
       case 2:
-        drawTile(x, y, "brown"); // bricks
+        drawTile(x, y, tileX[2], tileY[0]); // bricks
         break;
       case 3:
-        drawTile(x, y, "yellow"); // question box
+        drawTile(x, y, tileX[24], tileY[0]); // question box
+        break;
+      case 4:
+        drawTile(x, y, tileX[0], tileY[9]); // pipe left
+        break;
+      case 5:
+        drawTile(x, y, tileX[0], tileY[8]); // pipe top left
+        break;
+      case 6:
+        drawTile(x, y, tileX[1], tileY[8]); // pipe top right
+        break;
+      case 7:
+        drawTile(x, y, tileX[1], tileY[9]); // pipe right
         break;
     }
   }
 }
 
-// draws a tile rectangle and fill it with respective color
-function drawTile(x, y, type) {
-  c.beginPath();
-  c.rect(x * tileSize, y * tileSize, tileSize, tileSize);
-  c.fillStyle = type;
-  c.fill()
-  c.closePath();
+// draws the actual tile
+function drawTile(x, y, inputX, inputY) {
+
+  //tileSize = size of tile. inputX,Y = where from tilemap to crop. x,y = where to place tile.
+  c.drawImage(tileSet, inputX, inputY, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
 }
+
+
+} //////////// End onload function //////////////
