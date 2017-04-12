e27dede08acb0f78f4fdbbb38e436e5dd8739bd8
Zyberg <niko.eimutis.niko1@gmail.com>
2017-04-12 17:58:52 +0300

    Images Loading, working movement(no animation), added more functions for reusability

--- Bin/map.js
+++ Bin/map.js
@@ -1,33 +1,7 @@
-var tileSet = new Image();
-tileSet.src = "Assets/img/Tileset.png";
+var tileSet = images[0];
 
-////////// Begin create map when image is loaded. /////////
-// TODO: Better way to preload image?
-
-tileSet.onload = function() {
-
-// store tile data in 2 arrays. If you look at tileset, tileX and tileY would correspond to the coordinates of said tile.
-// TODO: Maybe do this with 2d array instead? Not sure how though
-
-var tileX = [],
-    tileY = [];
-
-for (var i = 0; i < tileSet.width/16; i++) {
-  tileX[i] = i * 16;
-}
-
-for (var i = 0; i < tileSet.height/16; i++) {
-  tileY[i] = i * 16;
-}
-
-
-// tile size
-var tileSize = canvas.height / 16; // Game map is 16 tiles high
-
-
-// Background 2D array
-// screen is 16 x 20 tiles (640 x 800px of 40px each)
-
+//Map is a 2D array
+//Screen is 16 x 20 tiles (640 x 800px of 40px each)
 var map = [
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -47,42 +21,56 @@ var map = [
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
-// Loop that fills in tiles from map data
+var tileSize = canvas.height / 16; // Game map is 16 tiles high
 
-for (var y = 0; y < map.length; y++) {
-  for (var x = 0; x < map[y].length; x++) {
-    switch (map[y][x]) {
-      case 1:
-        drawTile(x, y, tileX[0], tileY[0]); // ground
-        break;
-      case 2:
-        drawTile(x, y, tileX[2], tileY[0]); // bricks
-        break;
-      case 3:
-        drawTile(x, y, tileX[24], tileY[0]); // question box
-        break;
-      case 4:
-        drawTile(x, y, tileX[0], tileY[9]); // pipe left
-        break;
-      case 5:
-        drawTile(x, y, tileX[0], tileY[8]); // pipe top left
-        break;
-      case 6:
-        drawTile(x, y, tileX[1], tileY[8]); // pipe top right
-        break;
-      case 7:
-        drawTile(x, y, tileX[1], tileY[9]); // pipe right
-        break;
+// store tile data in 2 arrays. If you look at tileset, tileX and tileY would correspond to the coordinates of said tile.
+// TODO: Maybe do this with 2d array instead?
+var tileX = [],
+    tileY = [];
+
+function tilesInit() {
+    for (var i = 0; i < tileSet.width/16; i++) {
+      tileX[i] = i * 16;
     }
-  }
+    for (var i = 0; i < tileSet.height/16; i++) {
+      tileY[i] = i * 16;
+    }
 }
 
 // draws the actual tile
 function drawTile(x, y, inputX, inputY) {
-
-  //tileSize = size of tile. inputX,Y = where from tilemap to crop. x,y = where to place tile.
-  c.drawImage(tileSet, inputX, inputY, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
+    //tileSize = size of tile. inputX,Y = where from tilemap to crop. x,y = where to place tile.
+    c.drawImage(tileSet, inputX, inputY, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
 }
 
-
-} //////////// End onload function //////////////
+// Loop that fills in tiles from map data
+//TODO: screenDraw function that draws just the screen, not whole map.
+function mapDraw() {
+    for (var y = 0; y < map.length; y++) {
+      for (var x = 0; x < map[y].length; x++) {
+        switch (map[y][x]) {
+          case 1:
+            drawTile(x, y, tileX[0], tileY[0]); // ground
+            break;
+          case 2:
+            drawTile(x, y, tileX[2], tileY[0]); // bricks
+            break;
+          case 3:
+            drawTile(x, y, tileX[24], tileY[0]); // question box
+            break;
+          case 4:
+            drawTile(x, y, tileX[0], tileY[9]); // pipe left
+            break;
+          case 5:
+            drawTile(x, y, tileX[0], tileY[8]); // pipe top left
+            break;
+          case 6:
+            drawTile(x, y, tileX[1], tileY[8]); // pipe top right
+            break;
+          case 7:
+            drawTile(x, y, tileX[1], tileY[9]); // pipe right
+            break;
+        }
+      }
+    }
+}
---
+++ Bin/player.js
@@ -0,0 +1,80 @@
+//TODO: Change mario sprites   
+var mario = images[1]
+
+
+//Mario Variables
+//X and Y position of Mario
+var marioX = 0, marioY = tileSize * 13;
+var marioDX = 2;
+
+//Sprites sheet Variables
+var spriteWidth = 16, spriteHeight = 16;
+var framePosition = 64;		//Default Standing Mario
+var frameSprite = 0;		//Sprite that is going to get printed //TODO: Create array of positions, like with map
+
+function marioDraw(){
+    c.drawImage(mario, framePosition, 0, spriteWidth, spriteHeight, marioX, marioY, canvas.height / 16, canvas.height / 16);
+}
+
+//Mario Movement Variables
+var direction = 1; 	// 1 for right, -1 for left
+
+//Key Event Variables
+var rightPressed = false;
+var leftPressed = false;
+
+//TODO: Sprites changer
+function marioSprite(){
+	if(rightPressed && direction == 1)	{
+		framePosition = 80 + (16 * Math.floor((frameSprite % 3)));
+		frameSprite+= 0.2;
+	}
+	else if(leftPressed && direction == -1)	{
+		framePosition = 32 - (16 * Math.floor((frameSprite % 3)));
+		frameSprite+= 0.2;
+	}
+	else   {
+		if (direction == 1) {
+		framePosition = 64;
+		}
+		if(direction == -1){
+			framePosition = 48;
+		}
+	}
+	
+
+}
+
+function marioMove()	{
+	//Mario Moving Logic
+	if(rightPressed)	{
+		marioX += marioDX;
+		direction = 1;
+	}
+	else if(leftPressed)	{
+		marioX -= marioDX;
+		direction = -1;
+	}
+}
+
+//Keyboard Events
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
